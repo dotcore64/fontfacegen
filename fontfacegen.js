a30f67c9ac8c04d466082494866a92342ca4f4b0
Samuel Genest <is.genest@gmail.com>
2016-03-16 20:08:09 -0400

    Moved generateConfig() to lib/configure.js

--- fontfacegen.js
+++ fontfacegen.js
@@ -16,20 +16,17 @@ var
 fs                 = require('fs'),
 path               = require('path'),
 mkdirp             = require('mkdirp').sync,
+configure          = require('./lib/configure.js'),
 ttf2woff2          = require('ttf2woff2'),
 ttf2eot            = require('./lib/ttf2eot.js'),
 ttf2svg            = require('./lib/ttf2svg.js'),
-has                = require('./lib/helpers.js').has,
-quote              = require('./lib/helpers.js').quote,
-merge              = require('./lib/helpers.js').merge,
-isLinux            = require('./lib/helpers.js').isLinux(),
 fontforge          = require('./lib/fontforge.js'),
 stylesheets        = require('./lib/stylesheets.js'),
 
 // ----------------------------------------------------------------------------
 
 generateFontFace = function(options) {
-    var config = generateConfig(options);
+    var config = configure(options);
 
     mkdirp(config.dest_dir);
     mkdirp(path.dirname(config.css));
@@ -45,39 +42,6 @@ generateFontFace = function(options) {
 
 
 // ----------------------------------------------------------------------------
-
-generateConfig = function(options) {
-    var _ = {
-        source   : options.source,
-        dest_dir : options.dest,
-        collate  : options.collate || false
-    };
-
-    _.extension    = path.extname(_.source);
-    _.basename     = path.basename(_.source, _.extension);
-    _.dest_dir     = _.collate ? path.join(_.dest_dir, _.basename) : _.dest_dir;
-    _.target       = path.join(_.dest_dir, _.basename);
-    _.config_file  = _.source.replace(_.extension, '') + '.json';
-    _.ttf          = [_.target, '.ttf'].join('');
-    _.eot          = [_.target, '.eot'].join('');
-    _.svg          = [_.target, '.svg'].join('');
-    _.woff         = [_.target, '.woff'].join('');
-    _.woff2        = [_.target, '.woff2'].join('');
-    _.css          = [_.target, '.css'].join('');
-    _.css_fontpath = '';
-    _.name         = fontforge.getName(_.source);
-    _.weight       = fontforge.getWeight(_.source);
-    _.style        = fontforge.getStyle(_.source);
-    _.embed        = [];
-
-    if (fs.existsSync(_.config_file)) {
-        merge(_, JSON.parse(fs.readFileSync(_.config_file)));
-    }
-
-    merge(_, options);
-
-    return _;
-},
 
 generateTtf = function(config) {
 
